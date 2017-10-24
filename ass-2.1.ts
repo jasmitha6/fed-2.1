c83ff4ed1016a6c41cf059cd3fbd7cb842fc33fc
jasmitha <jasmieyjas@gmail.com>
2017-10-24 23:08:42 +0530

    Add files via upload

---
+++ ass-2.1.ts
@@ -0,0 +1,6 @@
+// generating arrow function
+var userDeatil = (userName: string , age: number) => {
+  return `My name is ${userName} and my age is ${age}`;
+};
+
+userDeatil("Jasmitha",24);  // invoking user detail fumction
